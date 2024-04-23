55e2ed5074077eed38f7ee5c1e853b6361abee6a
dean <tgabrelc@unca.edu>
2024-04-22 21:59:36 -0400

    tutorial 11

---
+++ tutorials/tutorial11/exercise01/main.js
@@ -0,0 +1,21 @@
+function makeRed() {
+    onclick
+    console.log('Change the box\'s background color to red');
+    document.querySelector("box").style.backgroundColor="red";
+}
+
+function makeBlue() {
+    onclick
+    console.log('Change the box\'s background color to blue');
+}
+
+function makePink() {
+    onclick
+    console.log('Change the box\'s background color to pink');
+}
+
+function makeOrange() {
+    onclick
+    console.log('Change the box\'s background color to orange');
+}
+
